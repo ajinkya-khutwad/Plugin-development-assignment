9f4a113341cadbcd4f8edf6c4be27eb607464f98
ajinkya-khutwad <ajinkya.khutwad@hbwsl.com>
2022-10-17 14:41:36 +0530

    rewrite the code using boilerplate

--- wp-content/plugins/wp-book/includes/class-wp-book.php
+++ wp-content/plugins/wp-book/includes/class-wp-book.php
@@ -161,6 +161,7 @@ class Wp_Book {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_admin, 'custom_post_type' );
 
 	}
 
