2eff3358e38608a9ce039f8eef9cd1f29003d55d
ajinkya-khutwad <ajinkya.khutwad@hbwsl.com>
2022-10-17 11:04:30 +0530

    Create a custom post type Book

---
+++ wp-content/plugins/wp-book/wp-book.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://wordpress1.to
+ * @since             1.0.0
+ * @package           Wp_Book
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Book
+ * Plugin URI:        https://wordpress1.to
+ * Description:       Plugin development assignment
+ * Version:           1.0.0
+ * Author:            Ajinkya Khutwad
+ * Author URI:        https://wordpress1.to
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-book
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WP_BOOK_VERSION', '1.0.0' );
+
+/**
+ * Plugin name constant.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ */
+define( 'WP_BOOK_NAME', 'wp-book' );
+
+/**
+ * Plugin base dir path.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * used to locate plugin resources primarily code file
+ */
+define( 'WP_BOOK_BASE_DIR', plugin_dir_path( __FILE__ ) );
+
+/**
+ * Plugin url.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * used to access its resources through Browser
+ */
+define( 'WP_BOOK_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-wp-book-activator.php
+ */
+function activate_wp_book() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-book-activator.php';
+	Wp_Book_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-wp-book-deactivator.php
+ */
+function deactivate_wp_book() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-book-deactivator.php';
+	Wp_Book_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_wp_book' );
+register_deactivation_hook( __FILE__, 'deactivate_wp_book' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wp-book.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wp_book() {
+
+	$plugin = new Wp_Book();
+	$plugin->run();
+
+}
+run_wp_book();
+
+// Register Custom Post Type
+function custom_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Books', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Book', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Book', 'text_domain' ),
+		'name_admin_bar'        => __( 'Book', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Books', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Book', 'text_domain' ),
+		'add_new'               => __( 'Add New Book', 'text_domain' ),
+		'new_item'              => __( 'New Book', 'text_domain' ),
+		'edit_item'             => __( 'Edit Book', 'text_domain' ),
+		'update_item'           => __( 'Update Book', 'text_domain' ),
+		'view_item'             => __( 'View Book', 'text_domain' ),
+		'view_items'            => __( 'View Books', 'text_domain' ),
+		'search_items'          => __( 'Search Book', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Books list', 'text_domain' ),
+		'items_list_navigation' => __( 'Books list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Books list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Book', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-book',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'book', $args );
+
+}
+add_action( 'init', 'custom_post_type', 0 );
+
